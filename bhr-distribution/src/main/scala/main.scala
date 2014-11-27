68e85cde3fc700122ae45c9deacee71e9b3b4eef
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-11-27 14:01:45 +0000

    Add uptime.

--- bhr-distribution/src/main/scala/main.scala
+++ bhr-distribution/src/main/scala/main.scala
@@ -27,27 +27,33 @@ object Analysis{
     implicit val sc = new SparkContext(conf)
     implicit lazy val formats = DefaultFormats
 
-    val pings = Pings("Firefox", "nightly", "*", "*", "20141106").RDD(0.2)
-    val processHangs = pings.map(ping => parse(ping.substring(37)) \ "threadHangStats").cache
+    val pings = Pings("Firefox", "nightly", "*", "*", "20141106").RDD(0.1)
+    val processHangs = pings.map(ping => {
+      val parsed = parse(ping.substring(37))
+      (parsed \ "threadHangStats", parsed \ "simpleMeasurements" \ "uptime")
+    }).cache
 
     val sessionHangs = processHangs.flatMap{
-      case JArray(list) => list
+      case (JArray(list), JInt(uptime)) => list.zip(Iterator.fill(list.size)(uptime).toList)
       case _ => Nil
-    }.filter(threadHangs =>
+    }.filter{ case(threadHangs, uptime) =>
       threadHangs \ "name" == JString("Gecko")
-    ).map(threadHangs => {
+    }.map{ case(threadHangs, uptime) => {
       val JArray(hangs) = threadHangs \ "hangs"
 
-      hangs.map(hang => {
+      val num = hangs.map(hang => {
         val JObject(bins) = hang \ "histogram" \ "values"
         val sum = bins.map{ case (bin, JInt(cnt)) => cnt }.sum
         assert(sum > 0)
         sum
       }).sum.toDouble
-    })
+
+      (num, uptime)
+    }}
 
     var writer = new PrintWriter(new File("session.csv" ))
-    sessionHangs.collect.foreach(x => writer.println(x))
+    writer.println("hangs,uptime")
+    sessionHangs.collect.foreach{ case(hangs, uptime) => writer.println(hangs + "," + uptime)}
     writer.close()
 
     sc.stop()
